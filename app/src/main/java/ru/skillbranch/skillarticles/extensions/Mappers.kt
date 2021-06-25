0321a8b97ad718f4779be4ae3a880f8dadef3539
Stepan Kim <kim.stepan@gmail.com>
2021-06-25 12:31:11 +0300

    hometask_delegates structure fixed

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/Mappers.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/Mappers.kt
@@ -1,6 +1,16 @@
 package ru.skillbranch.skillarticles.extensions
 
+import ru.skillbranch.skillarticles.data.AppSettings
+import ru.skillbranch.skillarticles.data.ArticlePersonalInfo
 import ru.skillbranch.skillarticles.viewmodels.ArticleState
+
+fun ArticleState.toAppSettings() : AppSettings {
+    return AppSettings(isDarkMode,isBigText)
+}
+
+fun ArticleState.toArticlePersonalInfo(): ArticlePersonalInfo {
+    return ArticlePersonalInfo(isLike, isBookmark)
+}
 
 fun ArticleState.asMap(): Map<String, Any?> = mapOf(
     "isAuth" to isAuth,
